b249882dca79066e81dca7fea01b7d882cf3d0e1
chenchongWeb <31728979+chenchongWeb@users.noreply.github.com>
2018-01-18 18:18:41 +0800

    0118

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -29,14 +29,14 @@
     height 44px
     line-height 44px
     font-size $font-size-medium
-      .tab-item
-        flex 1
-        text-align center
+    .tab-item
+      flex 1
+      text-align center
+      .tab-link
+        padding-bottom 5px
+        color $color-text-1
+      &.router-link-active
         .tab-link
-          padding-bottom 5px
-          color $color-text-1
-          &.router-link-active
-            .tab-link
-              color $color-theme
-              border-bottom 2px solid $color-theme
+          color $color-theme
+          border-bottom 2px solid $color-theme
 </style>
