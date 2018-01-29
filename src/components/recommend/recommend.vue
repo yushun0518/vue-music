fe7ccb032b30502cd7ca23875ea1ffaac0090f22
chenchongWeb <409602826@qq.com>
2018-01-29 23:47:05 +0800

    0129

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="recommend">
-      <scroll class="recommend-content" :data="discList">
+      <scroll ref="scroll" class="recommend-content" :data="discList">
+        <div>
           <div v-if="recommends.length" class="slider-wrapper">
             <slider>
               <div v-for="item in recommends">
                 <a :href="item.linkUrl">
-                  <img :src="item.picUrl">
+                  <img class="needsclick" @load="loadImage" :src="item.picUrl">
                 </a>
               </div>
             </slider>
           </div>
           <div class="recommend-list">
-              <h1 class="list-title">热门歌单推荐</h1>
-              <ul>
-                <li v-for="item in discList" class="item">
-                  <div class="icon">
-                    <img :src="item.imgurl" width="60" height="60">
-                  </div>
-                  <div class="text">
-                    <h2 class="name" v-html="item.creator.name"></h2>
-                    <p class="desc" v-html="item.dissname"></p>
-                  </div>
-                </li>
-              </ul>
+            <h1 class="list-title">热门歌单推荐</h1>
+            <ul>
+              <li v-for="item in discList" class="item">
+                <div class="icon">
+                  <img v-lazy="item.imgurl" width="60" height="60">
+                </div>
+                <div class="text">
+                  <h2 class="name" v-html="item.creator.name"></h2>
+                  <p class="desc" v-html="item.dissname"></p>
+                </div>
+              </li>
+            </ul>
           </div>
+        </div>
+        <div class="loading-container" v-show="!discList.length">
+          <loading></loading>
+        </div>
       </scroll>
   </div>
 </template>
@@ -33,6 +38,7 @@
   import Slider from 'base/slider/slider'
   import {getRecommend, getDiscList} from 'api/recommend.js'
   import {ERR_OK} from 'api/config.js'
+  import Loading from 'base/loading/loading'
 
   export default {
     data() {
@@ -60,11 +66,18 @@
             this.discList = res.data.list
           }
         })
+      },
+      loadImage() {
+        if (!this.checkloaded) {
+          this.checkloaded = true
+          this.$refs.scroll.refresh()
+        }
       }
     },
     components: {
       Slider,
-      Scroll
+      Scroll,
+      Loading
     }
   }
 </script>
