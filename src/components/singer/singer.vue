52d52cdcca718ccde7e1f438f55b1ed5dd5968e9
chenchongWeb <31728979+chenchongWeb@users.noreply.github.com>
2018-02-27 17:55:13 +0800

    chen

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -10,6 +10,7 @@
   import {ERR_OK} from 'api/config'
   import Singer from 'common/js/singer'
   import ListView from 'base/listview/listview'
+  import {mapMutations} from 'vuex'
 
   const HOT_SINGER_LEN = 10
   const HOT_NAME = '热门'
@@ -28,6 +29,7 @@
         this.$router.push({
           path: `/singer/${singer.id}`
         })
+        this.setSinger(singer)
       },
       _getSignerList() {
         getSingerList().then((res) => {
@@ -78,7 +80,10 @@
           return a.title.charCodeAt(0) - b.title.charCodeAt(0)
         })
         return hot.concat(ret)
-      }
+      },
+      ...mapMutations({
+        setSinger: 'SET_SINGER'
+      })
     },
     components: {
       ListView
